5036093a9a3f95933aeb501ac6a40d820204e251
IvanSm30 <98818416+IvanSm30@users.noreply.github.com>
2025-11-16 21:27:18 +0300

    Eda smirnov (#11)
    * Settings up project and added based eda for moex
    
    * Added different vizualization of moex_indices and moex_dividends
    
    * Fixed headers of graphics moex_dinidends
    
    * Added some changes for moex indices  and based analysis for t-pulse
    
    * inter res
    
    * Finalyze t-pulse visualization

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+
+
+def connection():
+    db_params = {
+        "dbname": "russian-stocks-prediction-ml-dl",
+        "user": "root",
+        "password": "groot",
+        "host": "185.70.105.233",
+        "port": "5432",
+    }
+    conn_str = f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}"
+    return create_engine(conn_str)
