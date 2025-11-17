30aae3ed86d963ae687b1c1f8bf579277d189193
Ivan Smirnov <smirnovivan1806@gmail.com>
2025-11-17 17:51:00 +0300

    Added script dayli load stock to db and set up env

--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def connection():
     db_params = {
-        "dbname": "russian-stocks-prediction-ml-dl",
-        "user": "root",
-        "password": "groot",
-        "host": "185.70.105.233",
-        "port": "5432",
+        "dbname": os.getenv("DB_NAME"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT"),
     }
     conn_str = f"postgresql+psycopg://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}"
     return create_engine(conn_str)
