c1472e76ae827a2121ab39ef9be0274a135ff375
Ivan Smirnov <smirnovivan1806@gmail.com>
2025-11-17 13:50:50 +0300

    Added libraries for working with ml and fixed connection to db

--- utils/utils.py
+++ utils/utils.py
@@ -9,5 +9,5 @@ def connection():
         "host": "185.70.105.233",
         "port": "5432",
     }
-    conn_str = f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}"
+    conn_str = f"postgresql+psycopg://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}"
     return create_engine(conn_str)
